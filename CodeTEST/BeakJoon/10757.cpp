46632dbb0dcb5f67c5827280f8022ef66120cf6b
cezyr0511 <cezyr0511@gmail.com>
2021-04-06 18:00:36 +0900

    큰 수 A+B

--- CodeTEST/BeakJoon/10757.cpp
+++ CodeTEST/BeakJoon/10757.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,57 +8,50 @@ using namespace std;
 //9223372036854775807 9223372036854775808
 //18446744073709551615
 
+
+int N, sum;
+int num1[10001], num2[10001];
+string s1, s2, tmp;
+vector<int> ans;
+
 int main()
-{	
-	string A, B;
+{
+	cin >> s1 >> s2;
 
-	cin >> A >> B;	
-
-	string At, Bt = "";
-
-	int Asize = A.length();
-	int Bsize = B.length();
-
-	//자리수 맞춰주기.
-
-	if (Asize >= Bsize)
+	// 더 긴 수를 s1으로 저장하기
+	if (s1.size() < s2.size())
 	{
-		for (int i = 0; i < Asize - Bsize; i++)
+		tmp = s1;
+		s1 = s2;
+		s2 = tmp;
+	}
+
+	// num1, num2 배열을 만드는 과정
+	for (int i = 0; i < s1.size(); i++)
+		num1[i + 1] = s1[i] - '0';
+
+	for (int i = 0; i < s2.size(); i++)
+		num2[i + 1 + (s1.size() - s2.size())] = s2[i] - '0';
+
+	// num배열들의 끝부분부터 덧셈을 하면서 ans벡터에 값 저장
+	for (int i = s1.size(); i > 0; i--)
+	{
+		sum = num1[i] + num2[i];
+		if (sum >= 10)
 		{
-			B.insert(0, "0");
+			num1[i - 1]++;
+			sum -= 10;
 		}
+		ans.push_back(sum);
 	}
-	else
+
+	// 맨 앞자리수 출력
+	if (num1[0] != 0) cout << 1;
+
+	// ans벡터 거꾸로 출력
+	for (int i = ans.size() - 1; i >= 0; i--)
 	{
-		for (int i = 0; i < Bsize - Asize; i++)
-		{
-			A.insert(0, "0");
-		}
+		cout << ans[i];
 	}
 
-	// ex) 1000000
-	//     0000010
-	//     1000010
-	//  
-
-	int Sum = 0;
-
-	string Result = "";
-
-	for (int i = Asize; i >= 0; i--)
-	{
-		Sum = (A[i] - '0') + (B[i] - '0');
-
-		if (Sum >= 10)
-		{		 
-			Result += Sum;
-
-			//cout << Result << endl;
-		}		
-
-	}
-
-	cout << A + B << endl;
-
-	return 0;
 }
