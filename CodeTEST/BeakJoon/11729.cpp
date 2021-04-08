86bb573d845c06d6b80e61d7aa0f713368cf8ce9
cezyr0511 <cezyr0511@gmail.com>
2021-04-08 18:57:27 +0900

    하노이 탑 이동 순서

--- CodeTEST/BeakJoon/11729.cpp
+++ CodeTEST/BeakJoon/11729.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
+void hanoi(int n, int start, int to, int bypass)
 {
-
-
-	return 0;
+    if (n == 1)
+        printf("%d %d\n", start, to);
+    else
+    {
+        hanoi(n - 1, start, bypass, to);
+        printf("%d %d\n", start, to);
+        hanoi(n - 1, bypass, to, start);
+    }
+}
+int main() {
+    int num;
+    cin >> num;
+    cout << (1 << num) - 1 << "\n";
+    hanoi(num, 1, 3, 2);
 }
