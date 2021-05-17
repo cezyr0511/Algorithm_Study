823cf5b5d0d3f8020a74778065c78e0e6c7db8ff
cezyr0511 <cezyr0511@gmail.com>
2021-05-17 18:21:52 +0900

    골드바흐의 추측

--- CodeTEST/BeakJoon/9020.cpp
+++ CodeTEST/BeakJoon/9020.cpp
@@ -3,26 +3,9 @@
 
 using namespace std;
 
-int main() 
-{   
-  /*  int count = 0;
+int main()
+{
 
-    for (int num = 1; num <= 1000000; num++) 
-    {
-        int sum = 0;
-        int max = num / 2;
 
-        for (int j = 1; j <= max; j++) 
-        {
-            if (num % j == 0)
-                sum += j;
-        }
-
-        if (num == sum)
-        {
-            cout << "¿ÏÀü¼ö : " << sum << endl;            
-        }
-    }*/
-   
-    return 0;
+	return 0;
 }
