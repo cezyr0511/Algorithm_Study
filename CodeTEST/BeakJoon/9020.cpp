7e3635f30dd052e560add7bf571f4286d0858902
cezyr0511 <cezyr0511@gmail.com>
2021-05-27 13:57:36 +0900

    골드바흐의 추측

--- CodeTEST/BeakJoon/9020.cpp
+++ CodeTEST/BeakJoon/9020.cpp
@@ -1,11 +1,53 @@
-#include <stdio.h>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main()
+bool isPrime(int i) 
 {
+	int rt;
 
+	rt = sqrt(i);
 
-	return 0;
+	if (rt == 1 && i != 1) 
+	{	
+		return true;
+	}
+
+	if (i % 2) 
+	{	
+		for (int j = 2; j <= rt; j++) 
+		{
+			if (i % j == 0)
+			{
+				return false;
+			}
+				
+			if (j == rt) 
+			{
+				return true;
+			}
+		}
+	}
+}
+
+int main() 
+{
+	int T;
+	cin >> T;
+
+	for (int Temp = 0; Temp < T; Temp++) 
+	{
+		int n;
+		cin >> n;
+
+		for (int i = n / 2; i >= 2; i--) 
+		{
+			if (isPrime(i) && isPrime(n - i)) 
+			{
+				cout << i << " " << n - i << endl;
+				break;
+			}
+		}
+	}
 }
