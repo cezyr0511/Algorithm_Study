9dfae75afd6cb764f7876a494d163ab153c8829c
cezyr0511 <cezyr0511@gmail.com>
2021-06-02 11:13:26 +0900

    덩치

--- CodeTEST/BeakJoon/7568.cpp
+++ CodeTEST/BeakJoon/7568.cpp
@@ -1,113 +1,45 @@
 #include <iostream>
-#include <map>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-typedef pair<char, int> p;
-
-bool cmp(const p& a, const p& b)
-{
-	if (a.second == b.second) 
-		return a.first < b.first;
-
-	return a.second > b.second;
-}
+typedef pair<int, int> p;
 
 int main()
 {
-	int n;
+	int num;
 
-	int Weight = 0, Height = 0;
+	int Rank = 1;
 
-	map<char, int> Weightdata;
-	map<char, int> Heightdata;
-	map<char, int> Rankdata;
+	p Arr[50];	
 
-	int Arr[50];
+	cin >> num;
 
-	fill_n(Arr, 50, 1);
+	vector<int> Rankvec;
 
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < num; i++)
 	{
-		cin >> Weight >> Height;
-		
-		Weightdata.insert({ (char)(i + 65), Weight });
-		Heightdata.insert({ (char)(i + 65), Height });
-		Rankdata.insert({ (char)(i + 65), 0 });		
+		cin >> Arr[i].first >> Arr[i].second;
 	}
 
-	vector<p> WeightVec(Weightdata.begin(), Weightdata.end());
-	vector<p> HeightVec(Heightdata.begin(), Heightdata.end());
-
-	sort(WeightVec.begin(), WeightVec.end(), cmp);
-	sort(HeightVec.begin(), HeightVec.end(), cmp);	
-
-	int Rank = n;
-
-	for (p H : HeightVec)
+	for (int i = 0; i < num; i++)
 	{
-		for (p R : Rankdata)
+		for (int j = 0; j < num; j++)
 		{
-			if (R.first == H.first && R.second != H.second)
+			if (Arr[i].first < Arr[j].first && Arr[i].second < Arr[j].second)
 			{
-				Rankdata[R.first] = Rank;
-
-				Rank--;
-			}
-		}
-	}
-
-	Rank = n;
-
-	for (p W : WeightVec)
-	{
-		for (p R : Rankdata)
-		{
-			if (R.first == W.first && R.second != W.second)
-			{
-				Rankdata[R.first] += Rank;
-
-				Rank--;
-			}
-		}
-	}
-	
-	int i = 0;
-
-	for (p a : Rankdata)
-	{
-		for (p b : Rankdata)
-		{
-			if (a.second < b.second)
-			{
-				Arr[i] ++;
+				Rank++;
 			}
 		}
 
-		i++;
+		Rankvec.push_back(Rank);
+
+		Rank = 1;
 	}
 
-	for (p a : WeightVec)
+	for (auto a : Rankvec)
 	{
-		cout << a.first << ":" << a.second << " ";
-	}
-
-	cout << endl;
-
-	for (p a : HeightVec)
-	{
-		cout << a.first << ":" << a.second << " ";
-	}
-
-	cout << endl;
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << Arr[i] << " ";
+		cout << a << " ";
 	}
 
 	return 0;
